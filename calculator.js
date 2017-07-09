33c427f62197e1ffd27a1806d095ed7dfbc2e1c5
Erbil <erbil.silik@yandex.com>
2017-07-10 01:33:48 +0300

    Full functionality given to the calculator (with the helps of my friend, @devcem

--- calculator.js
+++ calculator.js
@@ -14,15 +14,61 @@ function Calculator(){
 	}
 
 	this.addToScreen = function(value){
-		if (parseInt(value) && parseInt(this.screen[this.screen.length-1])){
-			this.screen[this.screen.length-1]+= value;
-		}
+		var lastElement = this.screen[this.screen.length-1];
 
-		else{
-			this.screen.push(value);
+		if(value == 'c'){
+			if(lastElement.length > 1){
+				lastElement.splice(lastElement.length-1, 1);
+			}else{
+				this.screen.splice(this.screen.length-1, 1);
+			}
+		}else if(value == '.'){
+			this.screen[this.screen.length-1]+=value;
+		}else if(parseFloat(value) && parseFloat(this.screen[this.screen.length-1])){
+			this.screen[this.screen.length-1]+=value;
+		}else{
+			if(value != lastElement){
+				this.screen.push(value);
+			}
 		}
 
 		this.update();
+	}
 
+	this.clean = function(){
+		this.screen = [];
+		this.update();
+	}
+
+	this.power = function(){
+		this.screen[this.screen.length-1] = this.screen[this.screen.length-1]*this.screen[this.screen.length-1];
+		this.update();
+	}
+
+	this.equal = function(){
+		var output = 0;
+		var lastFunction = '';
+
+		for(digit in this.screen){
+			part = this.screen[digit];
+
+			if(!parseFloat(part)){
+				lastFunction = part;
+			}else{
+				if(lastFunction == ''){
+					output = parseFloat(part);
+				}else if(lastFunction == '+'){
+					output+=parseFloat(part);
+				}else if(lastFunction == '-'){
+					output-=parseFloat(part);
+				}else if(lastFunction == '/'){
+					output/=parseFloat(part);
+				}else if(lastFunction == '*'){
+					output*=parseFloat(part);
+				}
+			}
+		}
+
+		this.screenElement.value = output;
 	}
 }
